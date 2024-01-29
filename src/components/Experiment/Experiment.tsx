9188aabf7fbaa53ba77c150ce46334e858bbe549
momogoyo <metheduxk@gmail.com>
2024-01-29 23:25:21 +0900

    ✨ Experiment 컴포넌트 추가

---
+++ src/components/Experiment/Experiment.tsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import Content from './components/Content'
+
+const Experiment = () => {
+  return (
+    <a>
+      <Header />
+      <Content />
+    </a>
+  )
+}
+
+export default Experiment
