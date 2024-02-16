11854724ee38f8e5a6efeaf519c209f4d275a55d
momogoyo <metheduxk@gmail.com>
2024-02-16 14:18:24 +0900

    🎸 다른 연구소 라우터 추가

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/kimbap">GO TO KIMBAP LABS</Link>
+      <Link href="/kimbap">🍙 KIMBAP LABS</Link>
+      <Link href="/sandwich">🥪 SANDWICH LABS</Link>
+      <Link href="/design">🎨 DESIGN LABS</Link>
+
       <div className="bg-[color:var(--turquoise)]">모모고요</div>
     </main>
   )
