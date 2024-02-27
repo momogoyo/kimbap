280e26caaffd113bef410658d070b4565b449aa5
momogoyo <metheduxk@gmail.com>
2024-02-27 23:45:26 +0900

    🍱 임시 샘플 이미지 사용을 위한 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,11 @@ import { withContentlayer } from 'next-contentlayer'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+
+  images: {
+    domains: ['static.oku.club', 'localhost']
+  }
 }
 
 export default withContentlayer(nextConfig)
