965a2ecfe2461883e550d94f2cfce171cff73f4e
momogoyo <metheduxk@gmail.com>
2024-03-03 00:33:38 +0900

    ✨ cn util 함수 추가

--- components/Mdx/Mdx.tsx
+++ components/Mdx/Mdx.tsx
@@ -1,5 +1,6 @@
 import { useMDXComponent } from 'next-contentlayer/hooks'
 import Image from 'next/image'
+import { cn } from '@/lib/utils'
 
 import Callout from '@/components/Callout'
 import MdxCard from '@/components/MdxCard'
@@ -11,61 +12,116 @@ interface ComponentProps {
 const components = {
   h1: ({ className, ...props }: ComponentProps) => (
     <h1 
-      className={`mt-2 scroll-m-20 text-4xl font-bold tracking-tight ${className}`}
+      className={cn(
+        "mt-2 scroll-m-20 text-4xl font-bold tracking-tight",
+        className
+      )}
       {...props}
     />
   ),
   h2: ({ className, ...props }: ComponentProps) => (
     <h2 
-      className={`mt-10 scroll-m-20 border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0 ${className}`}
+      className={cn(
+        "mt-10 scroll-m-20 border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0",
+        className
+      )}
       {...props}
     />
   ),
   h3: ({ className, ...props }: ComponentProps) => (
     <h3
-      className={`mt-8 scroll-m-20 text-2xl font-semibold tracking-tight ${className}`}
+      className={cn(
+        "mt-8 scroll-m-20 text-2xl font-semibold tracking-tight",
+        className
+      )}
       {...props}
     />
   ),
   h4: ({ className, ...props }: ComponentProps) => (
     <h4
-      className={`mt-8 scroll-m-20 text-xl font-semibold tracking-tight ${className}`}
+      className={cn(
+        "mt-8 scroll-m-20 text-xl font-semibold tracking-tight",
+        className
+      )}
       {...props}
     />
   ),
   h5: ({ className, ...props }: ComponentProps) => (
     <h5 
-      className={`mt-8 scroll-m-20 text-lg font-semibold tracking-tight ${className}`}
+      className={cn(
+        "mt-8 scroll-m-20 text-lg font-semibold tracking-tight",
+        className
+      )}
       {...props}
     />
   ),
   h6: ({ className, ...props }: ComponentProps) => (
     <h6 
-      className={`mt-8 scroll-m-20 text-lg font-semibold tracking-tight ${className}`}
+      className={cn(
+        "mt-8 scroll-m-20 text-lg font-semibold tracking-tight",
+        className
+      )}
       {...props}
     />
   ),
   a: ({ className, ...props }: ComponentProps) => (
-    <a className={`${className}`} {...props}/>
+    <a 
+      className={cn(
+        "font-medium underline underline-offset-4",
+        className
+      )}
+      {...props}
+    />
   ),
   p: ({ className, ...props }: ComponentProps) => (
-    <p className={`${className}`} {...props}/>
+    <p 
+      className={cn(
+        "leading-7",
+        className
+      )}
+      {...props}
+    />
   ),
   ul: ({ className, ...props }: ComponentProps) => (
-    <ul className={`${className}`} {...props}/>
+    <ul
+      className={cn(
+        "my-6 ml-6 list-disc",
+        className
+      )}
+      {...props}
+    />
   ),
   ol: ({ className, ...props }: ComponentProps) => (
-    <ol className={`${className}`} {...props}/>
+    <ol className={cn(
+        "my-6 ml-6 list-decimal",
+        className
+      )} 
+      {...props}
+    />
   ),
   li: ({ className, ...props }: ComponentProps) => (
-    <li className={`${className}`} {...props}/>
+    <li className={cn(
+        "mt-2",
+        className
+      )}
+      {...props}
+    />
   ),
   img: ({ className, alt, ...props }: React.ImgHTMLAttributes<HTMLImageElement>) => (
-    <img className={`rounded-md border ${className}`} alt={alt} {...props}/>
+    <img className={cn(
+      "rounded-md border",
+      className
+      )}
+      alt={alt}
+      {...props}
+    />
   ),
   code: ({ className, ...props}: ComponentProps) => (
     <code 
-      className={`relative rounded border px-[0.3rem] py-[0.2rem] font-mono text-sm ${className}`}
+      className={cn(
+        "relative rounded border px-[0.3rem] py-[0.2rem] font-mono text-sm",
+         className
+      )}
       {...props}
     />
   ),
@@ -73,7 +129,6 @@ const components = {
   Callout,
   Card: MdxCard
 }
-
 
 interface MdxProps {
   code: string
