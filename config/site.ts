c95f1e2711ce3c57965f2c030b5ca97c577e526e
momogoyo <metheduxk@gmail.com>
2024-08-23 00:30:27 +0900

    🐛 SiteConfig 수정

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '@/types'
 
 export const siteConfig: SiteConfig = {
   name: '윶',
-  description: '웹 개발자이자 화백',
+  description: 'UX엔지니어 그리고 화백',
   url: "https://avatars.githubusercontent.com/u/131684279?v=4",
   ogImage: "https://avatars.githubusercontent.com/u/131684279?v=4",
   links: {
