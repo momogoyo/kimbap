bd7e1bdc797c5c90376c7c62d5409bae600f37ef
momogoyo <metheduxk@gmail.com>
2024-08-23 00:38:38 +0900

    🎨 test content 삭제, siteconfig 수정

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '@/types'
 
 export const siteConfig: SiteConfig = {
   name: '윶',
-  description: 'UX엔지니어 그리고 화백',
+  description: 'UX엔지니어 그리고 김밥',
   url: "https://avatars.githubusercontent.com/u/131684279?v=4",
   ogImage: "https://avatars.githubusercontent.com/u/131684279?v=4",
   links: {
